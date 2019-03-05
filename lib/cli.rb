2e6fa1142bc17a0434366af021b39f3766486e31
David Korff <dkorff12@gmail.com>
2019-03-05 16:35:11 -0500

    Refactored add and remove user to repo

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,6 +2,7 @@ class CommandLineInterface
   def greet
     puts "Welcome to the GitHub Repo Explorer"
     puts "We can help you find your github repos."
+    # puts "Type 'exit' at any time to close the application."
   end
 
   def gets_user_input
@@ -20,18 +21,13 @@ class CommandLineInterface
     if username_exists?(github_username)
       User.all.find_by(github_username: github_username)
     else
-      puts "That user doesn't exist! (or doesn't exist by that exact username)"
+      false
     end
   end
 
   def run
     greet
     menu
-    # input = gets_user_input
-    # user = find_user(input)
-    # repos = find_repos(user)
-    # show_repos(repos)
-    # puts find_repo_by_keyword("First")[0].project_name
   end
 
   def menu
@@ -39,53 +35,75 @@ class CommandLineInterface
     puts "1. Find all projects by username"
     puts "2. Find all projects with a keyword"
     puts "3. Find all collaborators for a project"
-      # 3a. "Enter a project name"
-      # 4.
+          # *TO DO* add functionality to add users to project
     main_menu_loop
   end
-  #
+
+  def find_by_username_menu
+    puts "Enter a github username **WITH EXACT CAPITALIZATION** to list that user's projects:"
+    input = gets_user_input
+    if find_user(input) == false
+      puts "That user doesn't exist! (or doesn't exist by that exact username)"
+      menu
+    else
+      @user = find_user(input)
+    end
+    @repos = find_repos(@user)
+    if show_repos(@repos) == false
+      puts "User has no repos"
+      menu
+    end
+    select_repo_by_username_menu
+  end
+
+  def select_repo_by_username_menu
+    puts "Select repo number to view repo details"
+    input = gets_user_input
+    @selected_repo = @repos[input.to_i - 1]
+    @user_repo = find_user_repo(@user, @selected_repo)
+    puts "Repo name: #{@repos[input.to_i - 1].project_name}"
+    puts "Description: #{@repos[input.to_i - 1].description}"
+    puts "What would you like to do?"
+    puts "1. Remove current user from repo"
+    puts "2. Add another user to repo"
+    puts "3. Delete repo"
+  end
+
+  def remove_user_from_repo
+    UserRepo.destroy(@user_repo.id)
+    puts "Deleted #{@user.name} from #{@selected_repo.project_name}!"
+    menu
+  end
+
+  def add_user_to_repo
+    puts "Who do you want to add? (enter username with *EXACT* spelling and capitalization):"
+    input = gets_user_input
+    # puts input
+    @user = find_user(input)
+    if username_exists?(@user.github_username)
+      if already_on_repo?(@user, @selected_repo)
+        puts "#{@user.name} is already working on #{@selected_repo.project_name}"
+      else
+        @user.repos << @selected_repo
+        puts "#{@user.name} was successfully added to #{@selected_repo.project_name}"
+      end
+    end
+    menu
+  end
+
   def main_menu_loop
     while user_input != "exit"
       case @last_input.to_i
       when 1
-        puts "Enter a github username **WITH EXACT CAPITALIZATION** to list that user's projects:"
-        input = gets_user_input
-        user = find_user(input)
-        repos = find_repos(user)
-        show_repos(repos)
-        puts "Select repo number to view repo details"
-        input = gets_user_input
-        selected_repo = repos[input.to_i - 1]
-        user_repo = find_user_repo(user, selected_repo)
-        # puts "Repo name:" + repos[input.to_i - 1].project_name + " " + repos[input.to_i - 1].description
-        puts "Repo name: #{repos[input.to_i - 1].project_name}"
-        puts "Description: #{repos[input.to_i - 1].description}"
-        puts "What would you like to do?"
-        puts "1. Remove current user from repo"
-        puts "2. Add another user to repo"
-        puts "3. Delete repo"
+        find_by_username_menu
         while user_input != "exit"
           case @last_input.to_i
           when 1
             # Remove user from repo
-            UserRepo.destroy(user_repo.id)
-            puts "Deleted #{user.name} from #{selected_repo.project_name}!"
-            break
+            remove_user_from_repo
           when 2
             # Add user to repo
-            puts "Who do you want to add? (enter username with *EXACT* spelling and capitalization):"
-            input = gets_user_input
-            puts input
-            user = find_user(input)
-            if username_exists?(user.github_username)
-              if already_on_repo?(user, selected_repo)
-                puts "#{user.name} is already working on #{selected_repo.project_name}"
-              else
-                user.repos << selected_repo
-                selected_repo.users.each {|user| puts user.github_username}
-              end
-            end
-            break
+            add_user_to_repo
           when 3
             # Delete Repo
             puts "Are you sure you want to delete #{selected_repo.project_name}? (y/n)"
@@ -173,11 +191,14 @@ class CommandLineInterface
   end
 
   def show_repos(repos)
-
-    repos.each_with_index do |repo, index|
-      puts "#{index+1}. #{repo.project_name}"
+    if repos.length == 0
+      # puts "#{@user.name} has no repos"
+      false
+    else
+      repos.each_with_index do |repo, index|
+        puts "#{index+1}. #{repo.project_name}"
+      end
     end
-
   end
 
   def find_repo_by_keyword(keyword)
